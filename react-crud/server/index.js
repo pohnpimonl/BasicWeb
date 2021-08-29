8fcc2b31a5228d8105f8b372edf7e5391d264df4
pornpimon1987 <aweqaweq@hotmail.com>
2021-08-30 01:13:01 +0700

    sync dababase

--- react-crud/server/index.js
+++ react-crud/server/index.js
@@ -3,3 +3,27 @@ const app = express()
 const mysql = require('mysql')
 const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "M4553429y",
+    database: "employeeSystem"
+
+})
+
+app.get('/employee', (req, res) =>{
+    db.query("SELECT * FROM employee", (err, result) =>{
+        if (err){
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+app.listen('3001', () =>{
+    console.log('Server is running on port 3001')
+})
