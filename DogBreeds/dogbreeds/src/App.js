36891edc007220c2665d83e5a4035d356ebfcd49
pornpimon1987 <aweqaweq@hotmail.com>
2021-09-06 00:24:10 +0700

    dog breeds

---
+++ DogBreeds/dogbreeds/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+    <header className="App-header">
+      <h4>Dog Breeds</h4>
+    </header>
+      <div className="App-grid">
+        <div className="App-box">
+          <img classname="App-Pic" src="\pic\01Bulldog_L.jpg"/>
+          <h4>Bulldog</h4>
+        </div>
+        <div className="App-box">
+          <img classname="App-Pic" src="\pic\02poodle_L.jpg"/>
+          <h4>Bulldog</h4>
+        </div>
+        <div className="App-box">
+          <img classname="App-Pic" src="\pic\03german-shepherd_L.jpg"/>
+          <h4>Bulldog</h4>
+        </div>
+        <div className="App-box">
+          <img classname="App-Pic" src="\pic\01Bulldog_L.jpg"/>
+          <h4>Bulldog</h4>
+        </div>
+      </div>
+  </div>
+  );
+}
+
+export default App;
