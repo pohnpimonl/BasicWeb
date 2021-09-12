0873d4f585d4fb718c7ed0163d2f610df5e3b2b8
pornpimon1987 <aweqaweq@hotmail.com>
2021-09-13 00:14:33 +0700

    stair

--- Basic/BasicProgramming/BasicProgramming/Program.cs
+++ Basic/BasicProgramming/BasicProgramming/Program.cs
@@ -8,10 +8,23 @@ namespace BasicProgramming
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j =0; j<6;j++)
+                {
+                    if(i>=j)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
             Console.ReadLine();
         }
     }
 }
+
